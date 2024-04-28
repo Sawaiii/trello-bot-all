30a0f2190df14728c3e65c10f8b86ad82f394199
Sawaii <133211004+Sawaiii@users.noreply.github.com>
2024-04-28 19:11:37 +0300

    without information config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-telegram_api_token = "6560049046:AAEbWIGcTxHXTcTAEwSHJYopvatOZ2igcLM"
-trello_key = "57a45af296f110974f33f9a8f0b5456c"
-trello_token = "ATTA2b4b05867dae806b67d3149264da76e4c4011d02accb8988b966e9db36053a81E88ED71C"
-board_id = "jiMjxmZs"
-peer_id = -911014893
+telegram_api_token = " "
+trello_key = " "
+trello_token = " "
+board_id = " "
+peer_id = -
 delay = 5
